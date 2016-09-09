35262858995ba95098d1d4a82e510e8a4e9187c4
Tayfun Wiechert <tayfun.wiechert@googlemail.com>
2016-09-09 18:20:32 +0200

    - Cleaned library, removed unrequired stuff

--- src/main/java/de/twiechert/linroad/jdriver/DataDriver.java
+++ src/main/java/de/twiechert/linroad/jdriver/DataDriver.java
@@ -7,76 +7,20 @@ import com.sun.jna.Native;
  */
 public class DataDriver {
 
-    public enum Architecture {
-        X64_LINUX("datadriver"), X64_POWER_7("datadriverpower7"), X64_MACOS("macos");
+    private static final String LIB_NAME = "datadriver";
+    private DataDriverLibrary instance;
 
-        String file;
-
-        Architecture(String file) {
-            this.file = file;
-        }
-
-        public String getFile() {
-            return file;
-        }
-    }
-
-    // provide as arguement
-    public static void main(String[] args) {
-     DataDriver dataDriver = new DataDriver(args[0]);
-        dataDriver.runSample();
-    }
-
-     private DataDriverLibrary instance;
-     private String filePath;
-
-
-    public DataDriver(String filePath, String searchPath) {
-        System.setProperty("jna.library.path", searchPath);
-        this.filePath = filePath;
-        this.instance =  (DataDriverLibrary) Native.loadLibrary(Architecture.X64_LINUX.getFile(), DataDriverLibrary.class);
-
-    }
-
-    public DataDriver(String filePath, Architecture architecture) {
-        this.filePath = filePath;
-        this.instance =  (DataDriverLibrary) Native.loadLibrary( architecture.getFile(), DataDriverLibrary.class);
-    }
-
-
-    public DataDriver(String filePath, String searchPath, Architecture architecture) {
-        System.setProperty("jna.library.path", searchPath);
-        this.filePath = filePath;
-        this.instance =  (DataDriverLibrary) Native.loadLibrary( architecture.getFile(), DataDriverLibrary.class);
-    }
 
     public DataDriver(String searchPath) {
-
         System.setProperty("jna.library.path", searchPath);
-        this.instance =  (DataDriverLibrary) Native.loadLibrary(Architecture.X64_LINUX.getFile(), DataDriverLibrary.class);
-
+        this.instance =  (DataDriverLibrary) Native.loadLibrary(LIB_NAME, DataDriverLibrary.class);
     }
 
     public DataDriver() {
-        this.instance =  (DataDriverLibrary) Native.loadLibrary(Architecture.X64_LINUX.getFile(), DataDriverLibrary.class);
-    }
-
-    public void runSample() {
-        DataDriverLibrary.TupleReceivedCallback tupleReceivedCallback = new DataDriverLibrary.TupleReceivedCallback() {
-            public void invoke(String tuple) {
-                String[] array = tuple.split(",");
-                System.out.println("Received: "+tuple);
-                System.out.println("Current speed is: "+array[3]);
-
-            }
-        };
-
-        instance.startProgram(filePath,tupleReceivedCallback);
+        this.instance =  (DataDriverLibrary) Native.loadLibrary(LIB_NAME, DataDriverLibrary.class);
     }
 
     public DataDriverLibrary getLibrary() {return instance;}
-
-
 
 
 }
